5392ee5b34616cd58c8a6dd751a9296a1636422d
Pedro <pedrobeethoven8@gmail.com>
2019-10-01 20:56:18 -0300

    Add notations of request params and methods

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send('Has nothing here yet')
+// app.get('/', (req, res) => {
+//     return res.json({
+//         message: "has nothing here yet, keep calm"
+//     })
+// });
+
+// req.query = Acessar query params(para filtros)
+// req.params = Acessar route params(para edição e delete)
+// req.body = Acesar corpo da requisição(para criação, edição)
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+     return res.json(req.body);
 });
 
 app.listen(8888, console.log("Running on port 8888"));
