5cbf3c4d02800fdb161ed2d90c83881a7a3cba75
Pedro <pedrobeethoven8@gmail.com>
2020-01-11 13:41:26 -0300

    Update backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
+const path = require('path');
 
 mongoose
 	.connect(
@@ -21,8 +23,9 @@ mongoose
 // req.query = Acessar query params(para filtros)
 // req.params = Acessar route params(para edição e delete)
 // req.body = Acesar corpo da requisição(para criação, edição)
-
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(8888, console.log('Running on port 8888'));
